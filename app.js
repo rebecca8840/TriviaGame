22816f4cc6262fe491d2c0f2a0e6c8f525291076
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-01-20 22:30:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+$ ( document ).ready(function(){
+
+var questions = [
+  
+  {
+   q: "Which company issued the first hand held gaming unit?"
+   answers: ["Atari", "Nintendo", "Taito", "Mattel"] 
+   correctIndex: 3
+  },
+  
+  {
+   q: "The first arcade game (which was also the first commercially available video game) was released in what year?"
+   answers: ["1979", "1976", "1971", "1970"] 
+   correctIndex: 2
+  },
+
+    {
+   q: "''Houston, we've got a problem.'' On which spaceflight was this famous line uttered?"
+   answers: ["Apollo 11", "Apollo 13", "Gemini 8", "Enterprise"] 
+   correctIndex: 1
+  },
+
+    {
+   q: "Which of these was a popular fad toy that debuted with a long line of instrctions on how to care for it in 1975, and then practically disappeared in 1976??"
+   answers: ["Pound Puppies", "Chia Pets", "Pet Rock", "Raggedy Ann"] 
+   correctIndex: 2
+  },
+
+    {
+   q: "Which year was the first Star Wars film released?"
+   answers: ["1971", "1978", "1972", "1977"] 
+   correctIndex: 3
+  },
+
+    {
+   q: "What were the two most popular baby names in the 1970s?"
+   answers: ["Michael and Jennifer", "Alexander and Michelle", "Jason and Marie", "John and Olivia"] 
+   correctIndex: 0
+  },
+
+    {
+   q: "Which of these was not a top selling single in 1976?"
+   answers: ["'Fernando' - ABBA", "'We Are the Champions' - Queen", "'Dancing Queen' - ABBA", "'Don't Go Breaking My Heart' - Elton John and Kiki Dee"] 
+   correctIndex: 1
+  },
+
+    {
+   q: "In 1978, which country banned aerosol sprays due to environmental concerns?"
+   answers: ["Canada", "Sweden", "Norway", "Australia"] 
+   correctIndex: 1
+  },
+ ]
+
+
+var timer = {
+
+  var Timeout= setTimeout(start, 10000)
+
+      start: function() {
+      counter = setInterval(countdownTimer.count, 1000);
+
+
+}
+  
+
+
+
+
+});
+
